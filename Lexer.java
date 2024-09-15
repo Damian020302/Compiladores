c4dddf731b03fac97a60aaf05deb6b6fcac572fb
Damian020302 <damianvazqueztorrijos@ciencias.unam.mx>
2024-09-14 18:52:30 -0600

    Se agrega método para analizar una entrada

--- Lexer.java
+++ Lexer.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class Lexer{
+    /**
+     * Estados del AFD
+     */
     public enum Edo{
         q0,
         q1,
@@ -13,11 +16,51 @@ public class Lexer{
         q8
     }
 
+    /**
+     * Tokens
+     */
     public enum Token{
         ID,
         ENT,
         REAL,
         ESP,
         OP
+    }
+
+    public Edo edoActual;
+    public String entrada;
+    public int indice;
+    public StringBuilder valorToken;
+
+    /**
+     * Builder
+     */
+    public Lexer(String entrada){
+        this.entrada = entrada;
+        this.indice = 0;
+        this.edoActual = Edo.q0;
+        this.valorToken = new StringBuilder();
+    }
+
+    /**
+     * Analiza si el caracter es válido
+     */
+    public List<Token> analizar(){
+        List<Token> tokens = new ArrayList<>()
+        while(indice < entrada.length()){
+            char caracter = entrada.charAt(indice);
+            Estado edoSiguiente = edoSiguiente(caracter);
+            if(edoSiguiente == null){
+                throw new RuntimeException("Caracter no valido " + indice);
+            }
+            edoActual = edoSiguiente;
+            valorToken.append(caracter);
+            indice++;
+            if(esEdoAceptacion(edoActual)){
+                tokens.add(new Token(getTipoToken(edoActual), valorToken.toString()));
+                valorToken.setLength(0);
+            }
+        }
+        return tokens;
     }
 }
